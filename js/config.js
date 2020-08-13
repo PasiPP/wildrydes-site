0c49999d2f191a4ca94a1b20c289be4993ccf84b
Viskaali <pasi.poisuo@gmail.com>
2020-08-13 15:46:49 +0300

    updated pool 2

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_DuFg7gUZn
-        userPoolClientId: '', // e.g. 7gp0q15lu0839sunjmj8j18ed3
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_DuFg7gUZn', // e.g. us-east-1_DuFg7gUZn
+        userPoolClientId: '7gp0q15lu0839sunjmj8j18ed3', // e.g. 7gp0q15lu0839sunjmj8j18ed3
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
